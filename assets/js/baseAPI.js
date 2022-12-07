a7759beff117980c765e9fb79694e92b137344e9
nylhc <nylhc66@gmail.com>
2022-12-08 00:18:36 +0800

    已完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,21 @@
 $.ajaxPrefilter(function (options) {
     //拼接Ajax请求根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url
+
+    //统一为有权限接口设置请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //统一挂载complete回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //1.强制清空token
+            localStorage.removeItem('token')
+            //2.强制跳转
+            location.href = '/login.html'
+        }
+    }
 })
